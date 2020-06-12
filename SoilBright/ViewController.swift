263dc08f39ce10cc91d2159053f847829f5d8778
dbrickner3 <danbrickner@hotmail.com>
2020-06-12 14:59:20 -0400

    Initial Commit

---
+++ SoilBright/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoilBright
+//
+//  Created by Dan Brickner on 6/12/20.
+//  Copyright © 2020 Dan Brickner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
